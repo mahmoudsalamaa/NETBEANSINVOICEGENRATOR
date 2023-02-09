bc9d0dfe159dfb70b80119bae99f9502d5efa611
mahmoudsalama <mahmoudhgosalama@gmail.com>
2023-02-09 18:47:18 +0200

    first commit

---
+++ src/main/java/com/myinvoicegenerator/MyInvoiceGenerator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myinvoicegenerator;
+
+/**
+ *
+ * @author mahmoud
+ */
+public class MyInvoiceGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
